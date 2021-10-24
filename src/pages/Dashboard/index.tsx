3b61b2c394449faa12cfbcf13b09c6fc2b0730c2
Luis Sousa <lv-ss@hotmail.com>
2021-10-23 21:26:42 -0300

    criando pagina de dashboard

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,16 @@
+// import "./index.css";
+import Card from "../../components/Card";
+
+const Dashboard: React.FC = () => {
+  return (
+    <div className="Dashboard">
+      <div className="">
+        <h1>Dashboard</h1>
+      </div> 
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default Dashboard;
