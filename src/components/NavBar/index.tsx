2efbc89cf521255c1ce0755918b071a6f8ab561d
Luis Sousa <lv-ss@hotmail.com>
2021-10-24 00:40:36 -0300

    add props pro cypress

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,4 +1,3 @@
-
 import menu from "../../assets/icons/menu.svg";
 import invoice from "../../assets/icons/invoice.svg";
 import food from '../../assets/icons/food.svg';
@@ -8,13 +7,13 @@ import { NavBarContainer, NavItem } from './styled';
 const NavBar: React.FC = () => {
   return (
     <NavBarContainer>
-      <NavItem to="/">
+      <NavItem to="/" cy-data='nav-item--cardapio'>
         <img src={menu} alt="cardapio" width="35px" height="35px" />
       </NavItem>
-      <NavItem to="/fila">
+      <NavItem to="/fila" cy-data='nav-item--fila'>
         <img src={food} alt="fila" width="35px" height="35px" />
       </NavItem>
-      <NavItem to="/comanda">
+      <NavItem to="/comanda" cy-data='nav-item--comanda'>
         <img src={invoice} alt="comanda" width="35px" height="35px" />
       </NavItem>
     </NavBarContainer>
